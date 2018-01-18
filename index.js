4eaf6d0aa85e0382c21b420258d9e80f56bbe3e8
SteakOverCooked <Oh@SteakOverCooked.com>
2018-01-18 20:18:40 +0000

    remove examples

--- index.js
+++ index.js
@@ -102,10 +102,3 @@ const IntegerToEnglish = (x) => {
 module.exports = {
 	IntegerToEnglish 
 }
-
-console.log(IntegerToEnglish(0));
-console.log(IntegerToEnglish(1));
-console.log(IntegerToEnglish(11));
-console.log(IntegerToEnglish(1234));
-console.log(IntegerToEnglish(9999234));
-console.log(IntegerToEnglish(-9999234));
